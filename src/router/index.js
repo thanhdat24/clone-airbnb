9aa22b9512e9862321367a019defd52b454ffacb
Thành Đạt <54407533+thanhdat24@users.noreply.github.com>
2023-03-21 18:24:31 +0700

    Built: TheHeader

--- src/router/index.js
+++ src/router/index.js
@@ -1,34 +1,40 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
-import BlogView from "../views/BlogView.vue";
-import ContactView from "../views/ContactView.vue";
-import RoomsView from "../views/RoomsView.vue";
-import RoomDetailView from "../views/RoomDetailView.vue";
+import Home from "../views/Home.vue";
+import Blog from "../views/Blog.vue";
+import Contact from "../views/Contact.vue";
+import Rooms from "../views/Rooms.vue";
+import RoomDetail from "../views/RoomDetail.vue";
+import Auth from "../views/Auth.vue";
 const routes = [
   {
     path: "/",
     name: "home",
-    component: HomeView,
+    component: Home,
   },
   {
     path: "/blog",
     name: "Blog",
-    component: BlogView,
+    component: Blog,
   },
-   {
+  {
     path: "/contact",
     name: "Contact",
-    component: ContactView,
+    component: Contact,
   },
   {
     path: "/room",
     name: "Room",
-    component: RoomsView,
+    component: Rooms,
   },
   {
     path: "/room-detail",
     name: "RoomDetail",
-    component: RoomDetailView,
+    component: RoomDetail,
+  },
+  {
+    path: "/auth",
+    name: "Auth",
+    component: Auth,
   },
   {
     path: "/about",
@@ -37,13 +43,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+  linkActiveClass: "current",
 });
 
 export default router;
--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -1,97 +1,12 @@
 <template lang="">
-  <header id="header-container">
-    <!-- Header -->
-    <div id="header">
-      <div class="container">
-        <!-- Left Side Content -->
-        <div class="left-side">
-          <!-- Logo -->
-          <div id="logo">
-            <a href="index.html"
-              ><img
-                src="images/logo2.png"
-                data-sticky-logo="images/logo.png"
-                alt=""
-            /></a>
-          </div>
-
-          <!-- Mobile Navigation -->
-          <div class="mmenu-trigger">
-            <button class="hamburger hamburger--collapse" type="button">
-              <span class="hamburger-box">
-                <span class="hamburger-inner"></span>
-              </span>
-            </button>
-          </div>
-
-          <!-- Main Navigation -->
-          <nav id="navigation" class="style-1">
-            <ul id="responsive">
-              <li>
-                <router-link to="/">Home</router-link>
-              </li>
-              <li>
-                <router-link to="/blog">Blog</router-link>
-              </li>
-              <li>
-                <router-link to="/contact">Contact</router-link>
-              </li>
-            </ul>
-          </nav>
-          <div class="clearfix"></div>
-          <!-- Main Navigation / End -->
-        </div>
-        <!-- Left Side Content / End -->
-
-        <!-- Right Side Content / End -->
-        <div class="right-side">
-          <div class="header-widget">
-            <!-- User Menu -->
-            <div class="user-menu">
-              <div class="user-name">
-                <span><img src="images/dashboard-avatar.jpg" alt="" /></span>Hi,
-                Tom!
-              </div>
-              <ul>
-                <li>
-                  <a href="dashboard.html"
-                    ><i class="sl sl-icon-settings"></i> Dashboard</a
-                  >
-                </li>
-                <li>
-                  <a href="dashboard-messages.html"
-                    ><i class="sl sl-icon-envelope-open"></i> Messages</a
-                  >
-                </li>
-                <li>
-                  <a href="dashboard-bookings.html"
-                    ><i class="fa fa-calendar-check-o"></i> Bookings</a
-                  >
-                </li>
-                <li>
-                  <a href="index.html"
-                    ><i class="sl sl-icon-power"></i> Logout</a
-                  >
-                </li>
-              </ul>
-            </div>
-            <span>
-              <router-link to="/sign-in" class="sign-in popup-with-zoom-anim"
-                ><i class="sl sl-icon-login"></i> Sign In</router-link
-              >
-              <router-link to="/sign-up" class="sign-in popup-with-zoom-anim"
-                ><i class="sl sl-icon-user-follow"></i> Sign Up</router-link
-              >
-            </span>
-            <!-- <a href="dashboard-add-listing.html" class="button border with-icon"
-              >Add Listing <i class="sl sl-icon-plus"></i
-            ></a> -->
-          </div>
-        </div>
-        <!-- Right Side Content / End -->
-
-        <!-- Sign In Popup -->
-        <div id="sign-in-dialog" class="zoom-anim-dialog mfp-hide">
+  <!-- Sign In Popup -->
+  <div
+    class="mfp-wrap mfp-close-btn-in mfp-auto-cursor my-mfp-zoom-in mfp-ready my-bg"
+    tabindex="-1"
+  >
+    <div class="mfp-container mfp-inline-holder">
+      <div class="mfp-content">
+        <div id="sign-in-dialog" class="zoom-anim-dialog">
           <div class="small-dialog-header">
             <h3>Sign In</h3>
           </div>
@@ -220,15 +135,20 @@
             </div>
           </div>
         </div>
-        <!-- Sign In Popup / End -->
       </div>
     </div>
-    <!-- Header / End -->
-  </header>
-  <div class="clearfix"></div>
+  </div>
+  <!-- Sign In Popup / End -->
 </template>
 <script>
-import { RouterLink } from "vue-router";
 export default {};
 </script>
-<style lang=""></style>
+<style lang="scss">
+.my-bg {
+  background: url(https://images.hdqwalls.com/download/airbnb-in-the-90s-uc-2560x1024.jpg);
+  background-size: cover;
+  top: 0px;
+  position: absolute;
+  height: 100vh;
+}
+</style>
